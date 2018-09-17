57a7ed8a1aca3c81985c32d0813195f42f95018d
Ezra T Huffman <joshooha@gmail.com>
2018-09-17 19:49:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectrWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, key : value);
+  return Object.assign({}, object, {key : value});
 }
